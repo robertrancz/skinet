01cbaab5a66087aadbe10c39e16be12ad3795ccc
Robert Rancz <rancz.robi@gmail.com>
2022-05-05 14:36:07 +0300

    Create the not-found and server-error components

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
 
 
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    NavBarComponent,
+    NotFoundComponent,
+    ServerErrorComponent
   ],
   imports: [
     CommonModule,
