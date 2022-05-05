846166c6e99506a01dc9769bd1fd4ce23969f228
Robert Rancz <rancz.robi@gmail.com>
2022-05-05 15:16:44 +0300

    Add ngx-toastr to display bad request errors

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -4,6 +4,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
+import { ToastrModule } from 'ngx-toastr';
 
 
 
@@ -15,7 +16,11 @@ import { ServerErrorComponent } from './server-error/server-error.component';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true
+    })
   ],
   exports: [
     NavBarComponent
--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -3,11 +3,12 @@ import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/c
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private toastr: ToastrService) {
 
   }
 
@@ -15,6 +16,12 @@ export class ErrorInterceptor implements HttpInterceptor {
     return next.handle(request).pipe(
       catchError(error => {
         if(error) {
+          if(error.status === 400) {
+            this.toastr.error(error.error.message, error.error.statusCode);
+          }
+          if(error.status === 401) {
+            this.toastr.error(error.error.message, error.error.statusCode);
+          }
           if(error.status === 404) {
             this.router.navigateByUrl('/not-found');
           }
